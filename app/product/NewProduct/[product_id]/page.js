0634bf7ce972f7c0d16718c66f70201b767e04d6
fahizrahmank <fahizrahmank@gmail.com>
2023-09-18 07:51:39 +0530

    update

---
+++ app/product/NewProduct/[product_id]/page.js
@@ -0,0 +1,22 @@
+
+// for dynamic routing
+
+import React from 'react'
+
+const Page = () => {
+  return (
+<div className='flex-gap-5'>
+<div>here product image will come</div>
+<div>
+    <h2>product tittle</h2>
+    <p> lorem ipsum nfknfkjerkjreb</p>
+    <h4>price : $ 1000</h4>
+    <button>Add to cart</button>
+   
+</div>
+
+</div>
+    )
+}
+
+export default Page
---
+++ app/product/NewProduct/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewProduct = () => {
+  return (
+    <div>this is New Product</div>
+  )
+}
+
+export default NewProduct
